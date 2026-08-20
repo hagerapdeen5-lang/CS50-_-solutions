ba619d87d54904c072ec0ae5d11184c05e60ecb3
Hajar abdeen <hagerapdeen5@gmail.com>
2026-08-20 15:56:25 +0300

    Add types.c to demonstrate variable declarations

---
+++ types.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <cs50.h>
+int main(void){
+  int age = 20;
+  float GPA = 3.5;
+  char grade = 'a';
+  string name = "Hajar";
+  printf ("%s %i %f %c\n",name,age,GPA,grade);
+}
